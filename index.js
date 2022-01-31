4212a3e5abeacd697c65c69b022ab8ae7e3b3357
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-30 21:01:03 -0500

    commit tree 4a2453272e8b554ddc71fbd98fecc730dcd1448e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    messageDOM()
+  });
+  
+  function messageDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
